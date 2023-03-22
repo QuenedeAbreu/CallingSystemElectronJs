a9530449a90aa55fe0da9fd3c2704fc77c6e627e
Quenede Abreu <quenede.in@gmail.com>
2023-03-22 00:22:55 -0500

    update tabele chamados

--- src/ui/js/app.js
+++ src/ui/js/app.js
@@ -3,20 +3,31 @@ const main = require('@electron/remote').require('./main');
 const controllerChamado = main.controllerChamado;
 const ipc = window.require('electron').ipcRenderer;
 
+  // convert status num in circles color
+  const statusCirclesColort = (status) =>{
+    if(status === 0){
+      return gridjs.html('<i class="bi bi-code icon-status-open" data-bs-toggle="tooltip" data-bs-placement="top" title="Chamada aberto!"></i>')
+    }else if(status === 1){
+      return gridjs.html('<i class="bi bi-code-slash icon-status-close"data-bs-toggle="tooltip" data-bs-placement="top" title="Chamada concluido!"></i>')
+    }else {
+      return gridjs.html('<i class="bi bi-clock-history icon-status-wait" data-bs-toggle="tooltip" data-bs-placement="top" title="Chamada em espera de resposta!"></i>')
+    }
+  }
+
 // Busca todos os chamados
 let chamados = [];
 const getChamadoAll = async () => {
     try {
       const result = await controllerChamado.getChamadoAll()
       
-      result.map(chamado => {
+        result.map((chamado) => {
              chamados.push([chamado.id_chamado, 
               chamado.titulo_chamado,
               chamado.texto_chamado,
               chamado.data_chamado,
               chamado.atendente_chamado,
               chamado.pedido_chamado,
-              chamado.status
+             statusCirclesColort(chamado.status)
        ])
       })
       console.log(chamados);
@@ -27,9 +38,10 @@ const getChamadoAll = async () => {
     }
   }
 
+
+
 //rederiza a tabela 
 const renderTable = async (chamados) => {
-  
   const table = new gridjs.Grid({
     columns: ['Id','Titulo','Texto','Data','Atendente','A_pedido','Status'],
     data:chamados,
@@ -76,7 +88,7 @@ const renderTable = async (chamados) => {
       },
       td: {
         'text-align': 'center',
-        padding: 0
+        padding: "8px"
       },
       button: {
         color: "#fff"
@@ -84,17 +96,18 @@ const renderTable = async (chamados) => {
     }
   }).render(document.getElementById('table-wrapper'))
 
-
   //Inclusão do Botão para abrir o registro
   const gridjs_head = document.querySelector('.gridjs-head');
   gridjs_head.innerHTML = `<button onClick="openChildWindowRegister()" class="gridjs-head-button">
                           <i class="bi bi-plus"></i>
-                          </button>`
+                          </button>` 
 }
+
 
 //inicia todas funções
  async function Init(){
   await getChamadoAll();
+  await captureButton();
  }
  Init();
 //Abre a janela de registro
@@ -102,3 +115,22 @@ function openChildWindowRegister(){
     ipc.send('openChildWindowRegister');  
 }
 
+//Habilita o tooltip do bootstrap
+ const captureButton = async () => {
+    const teste = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    if(teste.length>0){
+      var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+      var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+      })
+
+    }else{
+      setTimeout(async () => {
+        await captureButton()
+      }, "900");    
+    }
+                     
+ 
+
+
+}
